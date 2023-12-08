41c962fb24916f11037fd63d71934d1771088e31
brian kramer <kramsman@yahoo.com>
2023-12-07 19:15:04 -0500

    adds creating factory var from pieces in factory_combined sheet

--- Work/Test combination of factory and county dictionaries dictify.py
+++ Work/Test combination of factory and county dictionaries dictify.py
@@ -1,8 +1,6 @@
 """ given lists of possible group elements, produce all grou combinations """
 
 import itertools
-import pathlib
-
 import pandas as pd
 from openpyxl import Workbook
 from loguru import logger
@@ -40,24 +38,6 @@ def get_file_name(box_title, title2, initial_dir):
 
     return file_name
 
-def read_sheet_w_header(ws):
-    ws_info = []
-    for row in ws.iter_rows(min_row=1, values_only=True):
-        ws_info.append(row)
-    ws_header = ws_info.pop(0)
-    return ws_header, ws_info
-
-
-def read_sheet_cols_into_lists(ws):
-    ws_info = []
-    ws_header = []
-    for col_tuple in ws.iter_cols(values_only=True):
-        col = list(col_tuple)  # cause tuples are immutable
-        col = [x for x in col if x is not None]
-        ws_header.append(col.pop(0))
-        ws_info.append(col)
-    # ws_header = ws_info.pop(0)
-    return ws_header, ws_info
 
 
 # age = ['Old', 'Young']
@@ -74,39 +54,22 @@ def read_sheet_cols_into_lists(ws):
 #                 ['Suffolk', '444-444-4444', 'nyvoter.net\suffolk'],
 #               ]
 
-if False:
-    wb_str = get_file_name("Select Input Workbook",
-                           f"Select factory and county input workbook.",
-                           "")
-else:
-    wb_str=  pathlib.Path("/Users/Denise/Library/CloudStorage/Dropbox/Postcard " \
-            "Files/PythonProgs/ROVCleaver_on_Dropbox/Work/factory_county_input.xlsx")
+wb_str = get_file_name("Select Input Workbook",
+                       f"Select factory and county input workbook.",
+                       "")
+wb = load_workbook(filename=wb_str)
+factory_sheet = wb["Factory"]
+county_sheet = wb["County"]
 
-wb_in = load_workbook(filename=wb_str)
-factory_sheet = wb_in["factory"]
-county_sheet = wb_in["county"]
-factory_parts = wb_in["factory_parts"]
-
-col_headers, col_lists = read_sheet_cols_into_lists(factory_parts)
-
-factory_combos = list(itertools.product(*col_lists))
-
-factory_combos2 = [["-".join(tuple1)] + list(tuple1) for tuple1 in factory_combos]
-factory_combos2.insert(0, ["factory"] + col_headers)
-
-# clear
-# write
-try:
-    del wb_in["factory_combined"]
-except:
-    pass
-wb_in.create_sheet(title="factory_combined")
-factory_combined = wb_in["factory_combined"]
-for row in factory_combos2:
-    factory_combined.append(row)
-# wb_in.save(wb_str)
-wb_in.save(wb_str)
-
+# for fn, format_flag, combine_flag, update_fn, update_fields, pull_group, custom_field, notes, *_ \
+#         in filelist_wks.iter_rows(min_row=2, values_only=True):
+# for row in factory_sheet.iter_rows(min_row=1, values_only=True):
+def read_sheet_w_header(ws):
+    ws_info = []
+    for row in ws.iter_rows(min_row=1, values_only=True):
+        ws_info.append(row)
+    ws_header = ws_info.pop(0)
+    return ws_header, ws_info
 
 factory_header, factory_info = read_sheet_w_header(factory_sheet)
 county_header, county_info = read_sheet_w_header(county_sheet)
@@ -120,23 +83,64 @@ county_header, county_info = read_sheet_w_header(county_sheet)
 #                 ['Asian-Old', 'hello wise asian person', 'social security', 'voting'],
 #               ]
 
+# county_info2 = example = [[x.replace('\r\n','') for x in l] for l in county_info]  # what to do with '\n' in
+# '\naussa'?
+
+#     county_dicts = {}
+#     for row in county_info:
+#         temp_dict = dict(zip(county_header, row))
+#         county_dicts[temp_dict['{county}']] = temp_dict
+
+def dictify(l_of_l: list[list], l_header: list[str], key_field: str) -> dict[dict]:
+    """ returns of dictionary of dictionaries given a list of lists, headers for the fields in the lists,
+    and the value to be used as the key into the final dictionary
+
+    Parameters
+    ----------
+    l_of_l : list of lists data, eg [['Kings:Young', 'nyvoter.net\kings'], ['Queens', 'nyvoter.net\queens']]
+    l_header : field names for each, eg ['county','url',]
+    key_field : field from headers to be used as the key into the sub lists, eg 'county'
+    """
+
+    dict_of_dicts = {}
+    for row in l_of_l:
+        temp_dict = dict(zip(l_header, row))
+        dict_of_dicts[temp_dict[key_field]] = temp_dict
+
+    return dict_of_dicts
+
+# county_dicts = dictify(county_info, county_header,'{county}')
+# factory_dicts = dictify(factory_info, factory_header,'{factory}')
+
 
 factory_county_list = list(itertools.product(factory_info, county_info))
 # factory_county_dict = list(itertools.product(factory_dicts, county_dicts))
 
 factory_county_list2 = [tuple1 + tuple2 for tuple1, tuple2 in factory_county_list]
 header2 = factory_header + county_header
-factory_county_list2.insert(0, header2)
+factory_county_list2.insert(0,header2)
 
-wb_out = Workbook()  # creates a workbook object.
-ws = wb_out.active  # creates a worksheet object.
+wb = Workbook()  # creates a workbook object.
+ws = wb.active  # creates a worksheet object.
 ws.title = "FacCount"
 for row in factory_county_list2:
     ws.append(row)  # adds values to cells, each list is a new row.
 
-wb_out.save('factory_county_output.xlsx')
+wb.save('factory_county_output.xlsx')
 
 a=1
+
+# for row in county_info
+#   for field in county_header
+#       output field[x]:row[x]
+
+# https://www.geeksforgeeks.org/python-convert-two-lists-into-a-dictionary/
+# res = {test_keys[i]: test_values[i] for i in range(len(test_keys))}
+
+# tuples = [(key, value) for i, (key, value) in enumerate(zip(test_keys, test_values
+# res = dict(tuples)
+
+# res = dict(zip(test_keys, test_values))
 
 group_fields = ['ethnicity', 'age']
 group_lists = [*group_fields]
@@ -152,12 +156,12 @@ all_data = [ [ethnicity, age, ethnicity + '-' + age] for ethnicity, age, county 
 # df.to_excel(writer, sheet_name='welcome', index=False)
 # writer.save()
 
-wb_in = Workbook()  # creates a workbook object.
-ws = wb_in.active  # creates a worksheet object.
+wb = Workbook()  # creates a workbook object.
+ws = wb.active  # creates a worksheet object.
 ws.append(['ethnicity', 'age', 'county', 'factory', 'campaign'])
 for row in all_data:
     ws.append(row)  # adds values to cells, each list is a new row.
 
-wb_in.save('all_groups.xlsx')
+wb.save('all_groups.xlsx')
 
 a=1
